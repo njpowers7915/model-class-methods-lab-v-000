6e7a0d309cc6d2ef2e7e3136d900dd1ab3763fb3
Nicholas Powers <njpowers7915@gmail.com>
2018-07-11 21:28:54 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,7 +4,7 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    first(5)
+    first(5).select(name)
     binding.pry
   end
 
