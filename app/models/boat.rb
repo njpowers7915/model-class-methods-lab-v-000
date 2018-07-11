62eac624f3b8a02438fb28fbfdd35bb000696ca5
Nicholas Powers <njpowers7915@gmail.com>
2018-07-11 21:45:23 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,28 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    pluck(:name).first(5)
+  end
+
+  def self.dinghy
+
+  end
+
+  def self.ship
+  end
+
+  def self.ast_three_alphabetically
+  end
+
+  def self.without_a_captain
+  end
+
+  def self.sailboats
+  end
+
+  def self.with_three_classifications
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,15 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def catamaran_operators
+  end
+
+  def sailors
+  end
+
+  def talented_seafarers
+  end
+
+  def non_sailors
+  end
 end
