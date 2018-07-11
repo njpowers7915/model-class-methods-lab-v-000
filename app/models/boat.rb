08b1bc9d767c1cd9bdecb17d83fcaf58992b7f84
Nicholas Powers <njpowers7915@gmail.com>
2018-07-11 21:18:41 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,10 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def first_five
+  end
+
+  def dinghy
+  end
 end
